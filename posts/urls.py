650a57712a161563805d7cddb1661682986ef998
BCSDev8 <bdev2743@gmail.com>
2024-01-30 12:58:16 +0600

    1

--- posts/urls.py
+++ posts/urls.py
@@ -4,9 +4,9 @@ from ncm_app.views import PageView
 from posts.views import PostDetailView, PostListView
 
 urlpatterns = [
-    path('page/<slug:slug>/', PageView.as_view(), name='page_view'),
+    # path('page/<slug:slug>/', PageView.as_view(), name='page_view'),
     path('post/list/', PostListView.as_view(), name='post_list'),
-    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_view'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 
     # Другие маршруты, если необходимо
 ]
